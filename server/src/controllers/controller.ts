5b15b576f81981e29b500aa594d8d9fad9f686d2
Frederic Bahr <fredericbahr1@gmail.com>
2022-04-20 10:05:44 +0200

    add prisma

--- server/src/controllers/controller.ts
+++ server/src/controllers/controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { httpOK } from "../config/statusCode";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const getHome = async (req: Request, res: Response) => {
   res.status(httpOK).json({ test: "Backend replied" });
